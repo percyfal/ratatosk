def8f68bce048f98ead69d8dba23120cd760483c
Per Unneberg <per.unneberg@scilifelab.se>
2013-03-17 19:24:32 +0100

    Remove scilife-specific code

--- ratatosk/scilife/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-import ratatosk
-# Define configuration file locations for predefined workflows
-config_dict = {
-    'seqcap' : os.path.join(ratatosk.__path__[0], os.pardir, "config", "seqcap.yaml"),
-    'haloplex' : os.path.join(ratatosk.__path__[0], os.pardir, "config", "haloplex.yaml"),
-    }
